134c930a9fa01b007eedee9d49616795e0490d96
Bryan <bryanzhu101@gmail.com>
2019-10-14 01:28:24 -0400

    hopefully 1 more time

---
+++ assign1/memgrind.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mymalloc.h"
+
+int main(){
+   
+    /*
+    //A:
+    int i = 0;
+    char *ptr;
+    while (i<150){
+        ptr = (char*) malloc(1);
+        printf("ptr is: %c\n", *ptr);
+        free(ptr);
+        i++;
+    }
+    printf("success for A:\n\n\n\n  ");
+    
+
+
+    //B:
+    int counter2 = 0;
+    char *ptr2;
+    char* array[50];
+    int indexCounter = 0;
+    while (counter2 < 150){
+        ptr2 = (char*)malloc(1);
+        array[indexCounter] = ptr2;
+        int freeCounter = 49;
+        if(indexCounter == 49){
+            while(freeCounter>=0){
+                free(&array[indexCounter]);
+                freeCounter--;
+            }
+            indexCounter = 0;
+            continue;
+        }
+        indexCounter++;
+        counter2++;
+       // printf("counter2: %d\n", counter2);
+        //printf("indexCounter: %d\n", indexCounter);
+    }   
+    printf("success for B:\n\n\n\n  ");
+
+
+    */
+    //C:
+    int numMalls = 0;
+    char* array2[50];
+    char* ptr3;
+    int indexCounter2 = 0;
+    int timez = 0;
+    srand(time(0));
+    int rando;
+    while(numMalls < 50){ 
+       
+        //if random number is even, we malloc
+        //if random number is odd, we free
+        rando = rand() % 2;
+        printf("rando: %d\n", rand());
+        if(rando == 0) {
+            ptr3 = (char*) malloc(1);
+            array2[indexCounter2] = ptr3;
+            numMalls++;
+            indexCounter2++;
+        }
+        else {
+            free(&array2[indexCounter2]);
+            if(array2[indexCounter2] != NULL) {
+                indexCounter2--;
+            }
+        }
+        timez++;
+    }
+    int counterTemp = 0;
+    while(counterTemp < 50) {
+        free(&array2[counterTemp]);
+        counterTemp++;
+    }
+    
+    char* element;
+    int loopC = 0;
+    while(loopC<350){
+        element = (char*) &arr[loopC];
+        printf("arr's 1st 350 elements: %c\n", *element);
+        loopC++;
+    }
+    //printf("array2's 0th index: %c\n", *array2[0]);
+   // printf("number of times really ran: %d\n", timez);
+   // printf("success for C: \n\n\n\n ");
+
+
+        
+
+    return 1;
+}
---
+++ assign1/mymalloc.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+#include "mymalloc.h"
+
+
+/*
+ * test cases:
+ *
+ * malloc(4096): y
+ * malloc(4097): n
+ * malloc(0): n
+ * malloc(1) 100 times: y
+ * malloc(4000) 2 times
+ */
+
+
+
+
+
+void* mymalloc(int x, const char* file, int line){
+
+    //printf("mymallocing ------: ");
+    
+    // metadata composed of 6 bytes: 4 bytes for magicNum, 2 bytes for how much user data
+    // x is out of range, -6 for metadata
+    if(x<=0 || x>4090){
+        return NULL;
+    }
+    
+    // declaring metadata
+    struct meta data;
+    data.magicNum = 10001;
+    data.used = (unsigned short) x;
+    
+    //'fresSpace' keeps track potential space for user data, also used for loop condition
+    int freeSpace = 0;
+
+    // 'i' iterates/jumps along arr, also used for loop condition, [0,4095]
+    int i = 0;
+    
+    // 'ptrTemp' reads an int from arr, used to check for magicNum
+    int *ptrTemp;
+
+    // 'usedTemp' reads short from arr, used to check for amt of user data and to jump
+    unsigned short *usedTemp;
+    
+    // run until found enough free space for metadata and userdata, and did not have to traverse out of bounds
+    while(freeSpace <= x + 6 && i<=4097){
+        
+        // assigns 'ptrTemp' to int pointer casted to arr at index i
+        ptrTemp = (int*)&arr[i];
+        
+        if(*ptrTemp == data.magicNum){//should be true after first time
+            freeSpace = 0;
+            usedTemp = (unsigned short*)&arr[i+4];
+            i = i + *usedTemp + 6;
+            continue;
+        }else{
+            freeSpace++;
+        }
+        i++;
+    }
+    // copies magicNum into arr
+    memcpy(&arr[i-x-7], (void *) &data, sizeof(data));
+    //printf("\nmetadata index: %p\n", &arr[i-x-7]);
+    //printf("This is i: %d\n",i);
+    // creates ptr to 6 bytes after metadata
+    if (i >  4097){
+        printf("\n------------null-----\n");
+        return NULL;
+    }
+    char* ptr = ((char *) &arr[i-x-1]);
+    
+    return (void*) ptr;
+
+}
+
+void  myfree(void* ptr, const char* file, int line){
+    // check if pointer, equal to NULL, beginning of memory, 
+    // check pointer points to our array
+    //printf("myfreeing----: ");
+    if(ptr == NULL) {
+        return;
+    }
+
+    if(!((char*)ptr<=&arr[4095] && (char*)ptr>=&arr[0])){
+        return;
+    }
+
+    // if -6, should be magic number
+    int *metaNum =  (int*)(ptr-6);
+    unsigned short* metaUsed = (unsigned short*)(ptr - 2);
+    //printf("This is .magicNum: %d\n", *metaNum);
+    //printf("This is .Used: %hi\n", *metaUsed);
+
+    if (*metaNum == 10001){
+        // set previous 6 and next shorts to 'a's'
+        char *i;
+        int loop = 0;
+        int stop = *metaUsed + 5;
+        for (i = (char*)(ptr-6); loop <= stop;i++){
+            *i='a';
+            loop++;
+        }
+    }else{
+        return;
+    }
+    
+    return;
+}
